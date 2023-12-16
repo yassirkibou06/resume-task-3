d5fd3964d5effdad215ae3bb0a768cb5a11ec095
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 17:28:22 +0100

    e

--- server/index.js
+++ server/index.js
@@ -14,20 +14,17 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const puppeteer = require('puppeteer');
-
 app.post('/create-pdf', async (req, res) => {
     const html = pdfPage(req.body);
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setContent(html);
-    await page.pdf({ path: 'Resume.pdf', format: 'A4' });
-    await browser.close();
-
-    res.status(200).send('PDF created successfully');
-});
-
-
+    pdf.create(html).toFile('Resume.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject());
+            console.log(err);
+        }
+        res.send(Promise.resolve());
+        console.log('PDF created');
+    })
+})
 
 app.get('/fetch-pdf', (req, res) => {
     res.sendFile(`${__dirname}/Resume.pdf`);
