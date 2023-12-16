497ee4b6a6a3845a9602f10b19fc765fb16fb61e
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 16:43:57 +0100

    add

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.get('/fetch-pdf', (req, res) => {
 app.use(express.static('../client/build'));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://resume-task-3-server.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
