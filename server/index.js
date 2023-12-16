467a480bc4dcd59bc4431852367d2ac406325c58
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 17:48:34 +0100

    hhhhhh

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ const pdfPage = require('./pdf');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
