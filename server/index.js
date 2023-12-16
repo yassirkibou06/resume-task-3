2d7d5d431c56e278118713848d58159584744675
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 17:42:57 +0100

    a

--- server/index.js
+++ server/index.js
@@ -7,18 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://resume-task-3-server.vercel.app',
+    origin: 'http://localhost:3000',
     credentials: true,
 }));
-
-app.use(function (req, res, next) {
-
-    res.header('Access-Control-Allow-Origin', "https://resume-task-3-server.vercel.app");
-    res.header('Access-Control-Allow-Headers', true);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
