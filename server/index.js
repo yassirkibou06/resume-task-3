a69398bb743e3cc30e580e511063dff55ee5dc45
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 18:02:56 +0100

    eee

--- server/index.js
+++ server/index.js
@@ -6,15 +6,7 @@ const pdfPage = require('./pdf');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
