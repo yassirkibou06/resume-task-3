541700a3cdf9d1d9b2c1e7498cbdf8256cf470c0
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-16 18:06:42 +0100

    ze

--- server/index.js
+++ server/index.js
@@ -7,29 +7,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/create-pdf', async (req, res) => {
-    const html = pdfPage(req.body);
-    pdf.create(html).toFile('Resume.pdf', (err) => {
-        if (err) {
-            res.send(Promise.reject());
-            console.log(err);
-        }
-        res.send(Promise.resolve());
-        console.log('PDF created');
-    })
-})
+    try {
+        const html = pdfPage(req.body);
+        pdf.create(html).toFile('Resume.pdf', (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Failed to create PDF' });
+            } else {
+                console.log('PDF created successfully');
+                res.status(200).json({ success: true });
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.get('/fetch-pdf', (req, res) => {
     res.sendFile(`${__dirname}/Resume.pdf`);
-})
+});
 
 app.use(express.static('../client/build'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
-
+});
