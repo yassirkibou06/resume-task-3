0f2b2991988e5de25074b894e00420f152b9dccd
yassirkibou06 <yassirkibou06@gmail.com>
2023-12-17 13:00:17 +0100

    aaesdd

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const pdf = require('html-pdf');
 const pdfPage = require('./pdf');
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
